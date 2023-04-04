e50cd69f539e7fdf38d19ecdfce905f200493535
Kody Brown <thewizard@wasatchwizard.com>
2023-04-04 12:19:03 -0600

    updated this website from kodybrown.com

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
 
+// main.js
+
+
+function addOnLoadEvent(func)
+{
+	var oldonload = window.onload;
+	if (typeof (window.onload) != "function") {
+		window.onload = func;
+	}
+	else {
+		window.onload = function ()
+		{
+			if (oldonload) {
+				oldonload();
+			}
+			func();
+		}
+	}
+}
+
+$(function () {
+  var includes = $('[data-include]')
+  $.each(includes, function () {
+    var file = 'views/' + $(this).data('include') + '.html'
+    $(this).load(file)
+  })
+})
